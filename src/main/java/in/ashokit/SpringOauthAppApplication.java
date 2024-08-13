747ac3741249728f2d2700ad8cf7fc6f98cc6554
shreyash <shreyash.sarve04@gmail.com>
2024-08-13 17:57:21 +0530

    fisrt commit

---
+++ src/main/java/in/ashokit/SpringOauthAppApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthAppApplication.class, args);
+	}
+
+}
